9cd2f5e5b46a66e3e1e3527f8601e3e2ed3ede2c
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-22 20:05:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,14 +23,14 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = nil)
+def can_be_created_in_a_block(args = {title: "Home ALone", release_date: "1990"})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = title
-    m.release_date = release_date
+    m.title = argrs[:title]
+    m.release_date = args[:release_date]
   end
 end
 
@@ -47,7 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
